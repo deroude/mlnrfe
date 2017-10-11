7946505c741f24367d485468ee0576514f398856
Valentin Raduti <valentin.raduti@computaris.com>
2017-10-11 11:49:02 +0300

    cleanup

--- functions/index.js
+++ functions/index.js
@@ -3,16 +3,6 @@ const functions = require('firebase-functions');
 const sendgrid = require('@sendgrid/mail');
 sendgrid.setApiKey(functions.config().sendgrid.key);
 
-function parseBody(body) {
-  var helper = sendgrid.mail;
-  var fromEmail = new helper.Email(body.from);
-  var toEmail = new helper.Email(body.to);
-  var subject = body.subject;
-  var content = new helper.Content('text/html', body.content);
-  var mail = new helper.Mail(fromEmail, subject, toEmail, content);
-  return mail.toJSON();
-}
-
 exports.httpEmail = functions.https.onRequest((req, res) => {
   return Promise.resolve()
     .then(() => {
